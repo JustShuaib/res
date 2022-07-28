2791651a932667dbeb4cb9db8d5a0454dca0ca79
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-07-28 09:09:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form-container");
+const email = document.getElementById("email");
+const inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit", displayError);
+
+function displayError(e) {
+  inputs.forEach((input) => {
+    const errLogo = input.nextElementSibling;
+    const errPara = errLogo.nextElementSibling;
+    if (input.value.length === 0) {
+      errLogo.classList.remove("hidden");
+      errPara.classList.remove("hidden");
+    }
+    if (input.value.length > 0) {
+      errLogo.classList.add("hidden");
+      errPara.classList.add("hidden");
+    }
+    if (!(email.value.includes("@") && email.value.includes("."))) {
+      email.nextElementSibling.classList.remove("hidden");
+      email.nextElementSibling.nextElementSibling.classList.remove("hidden");
+    }
+  });
+  e.preventDefault();
+}
